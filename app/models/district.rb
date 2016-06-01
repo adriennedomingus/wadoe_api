e0d76635f54833d796c4b492316fe2c6a5ddbca5
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-01 12:05:50 -0600

    first controllers and responses, and controller test

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,4 +1,22 @@
 class District < ActiveRecord::Base
   belongs_to :educational_service_district
   belongs_to :county
+
+  def self.all_districts
+    response = { districts: [
+      ]
+    }
+    all.each do |district|
+      response[:districts] << {
+        name: district.name,
+        slug: district.slug,
+        number: district.number,
+        educational_service_district: district.educational_service_district.name,
+        educational_service_district_slug: district.educational_service_district.slug,
+        county: district.county.name,
+        county_slug: district.county.slug,
+      }
+    end
+    response.to_json
+  end
 end
---
+++ spec/controller/districts_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::DistrictsController, type: :controller do
+  describe "GET index" do
+    it "returns all districts and their slugs" do
+      c = County.create(name: "King", number: "17", slug: "king")
+      esd = EducationalServiceDistrict.create(name: "Puget Sound Educational Service District 121", slug: "puget-sound-educational-service-district-121")
+      d1 = District.create(name: "Auburn School District", number: "17408", educational_service_district_id: esd.id, county_id: c.id, slug: "auburn-school-district")
+      d2 = District.create(name: "Issaquah School District", number: "17411", educational_service_district_id: esd.id, county_id: c.id, slug: "issaquah-school-district")
+
+      get :index, format: :json
+
+      districts = JSON.parse(response.body)
+
+      result = { "districts" =>
+                  [{ "name" => d1.name,
+                    "slug" => d1.slug,
+                    "number" => d1.number,
+                    "educational_service_district" => esd.name,
+                    "educational_service_district_slug" => esd.slug,
+                    "county" => c.name,
+                    "county_slug" => c.slug
+                  },
+                  { "name" => d2.name,
+                    "slug" => d2.slug,
+                    "number" => d2.number,
+                    "educational_service_district" => esd.name,
+                    "educational_service_district_slug" => esd.slug,
+                    "county" => c.name,
+                    "county_slug" => c.slug
+                  }]
+               }
+
+      expect(districts).to eq(result)
+    end
+  end
+end
