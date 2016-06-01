a0ac7f0d622fbf152de96297becb7dedd22c495f
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-01 16:06:06 -0600

    Redirect root path to api docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount SwaggerEngine::Engine, at: "/api-docs"
+  mount SwaggerEngine::Engine, at: '/api-docs'
+
+  root to: redirect('/api-docs')
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
