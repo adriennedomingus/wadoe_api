10ddf79437100a6fcfdc2434ab089367fa4d5e09
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-02 12:55:45 -0600

    Add swagger documentation for new endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/districts',                           to: 'districts#index'
-      get '/demographics/districts/:slug/:year', to: 'demographics/districts#show'
+      get '/demographics/districts',  to: 'demographics/districts#show'
     end
   end
 end
