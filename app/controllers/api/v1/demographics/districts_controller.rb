08eb0e6affd6bed3417b6fbea0a80f4365ef6588
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-02 10:57:21 -0600

    Error handling if school year and district combination doesn't exist in database

--- app/controllers/api/v1/demographics/districts_controller.rb
+++ app/controllers/api/v1/demographics/districts_controller.rb
@@ -4,7 +4,14 @@ class Api::V1::Demographics::DistrictsController < Api::ApiController
   def show
     district = District.find_by(slug: params["slug"])
     year = SchoolYear.find_by(years: params["year"])
-    se = StudentEnrollment.where(district_id: district.id, school_year_id: year.id)
-    respond_with se[0], serializer: DistrictDemographicsInYearSerializer
+    if year && district
+      se = StudentEnrollment.where(district_id: district.id, school_year_id: year.id)
+      respond_with se[0], serializer: DistrictDemographicsInYearSerializer
+    else
+      message = "We do not have data for that combination of school district and school year. Please try another query."
+      status = 400
+      response = { message: message, status: status }.to_json
+      respond_with response
+    end
   end
 end
--- spec/controller/demographics/districts_controller_spec.rb
+++ spec/controller/demographics/districts_controller_spec.rb
@@ -66,5 +66,47 @@ RSpec.describe Api::V1::Demographics::DistrictsController, type: :controller do
 
       expect(district_demographics).to eq(result)
     end
+
+    it "responds with error if school year doesn't exist" do
+      sy = SchoolYear.create(years: "2015-16")
+      c = County.create(name: "King", number: "17", slug: "king")
+      esd = EducationalServiceDistrict.create(name: "Puget Sound Educational Service District 121", slug: "puget-sound-educational-service-district-121")
+      d = District.create(name: "Auburn School District", number: "17408", educational_service_district_id: esd.id, county_id: c.id, slug: "auburn-school-district")
+      se = StudentEnrollment.create(school_year_id: sy.id, total: 1378, students_per_classroom_teacher: 18, district_id: d.id)
+      Gender.create(percent_male: 49, number_male: 73, percent_female: 51, number_female: 82, student_enrollment_id: se.id)
+      OtherDemographic.create(percent_migrant: 2.42, number_migrant: 73, percent_transitional_bilingual: 14.83, number_transitional_bilingual: 37, percent_frl: 37.34, number_frl: 145, percent_foster_care: 2.04, number_foster_care: 31, student_enrollment_id: se.id)
+      ExceptionalStudentService.create(percent_special_education: 10, number_special_education: 181, percent_504: 7.56, number_504: 147, student_enrollment_id: se.id)
+      RaceEthnicity.create(percent_american_indian_or_alaskan_native: 2.03, number_american_indian_or_alaskan_native: 28, percent_asian: 14.30, number_asian: 94, percent_pacific_islander: 2.32, number_pacific_islander: 83, percent_asian_pacific_islander: 8.32, number_asian_pacific_islander: 23, percent_black: 42.12, number_black: 123, percent_hispanic: 13.42, number_hispanic: 983, percent_white: 12, number_white: 193, percent_two_or_more: 12.3, number_two_or_more: 412, student_enrollment_id: se.id)
+
+      get :show, slug: d.slug, year: "2014-15", format: :json
+
+      expected_response = {
+        message: "We do not have data for that combination of school district and school year. Please try another query.",
+        status: 400
+      }.to_json
+
+      expect(response.body).to eq(expected_response)
+    end
+
+    it "responds with error if school district doesn't exist" do
+      sy = SchoolYear.create(years: "2015-16")
+      c = County.create(name: "King", number: "17", slug: "king")
+      esd = EducationalServiceDistrict.create(name: "Puget Sound Educational Service District 121", slug: "puget-sound-educational-service-district-121")
+      d = District.create(name: "Auburn School District", number: "17408", educational_service_district_id: esd.id, county_id: c.id, slug: "auburn-school-district")
+      se = StudentEnrollment.create(school_year_id: sy.id, total: 1378, students_per_classroom_teacher: 18, district_id: d.id)
+      Gender.create(percent_male: 49, number_male: 73, percent_female: 51, number_female: 82, student_enrollment_id: se.id)
+      OtherDemographic.create(percent_migrant: 2.42, number_migrant: 73, percent_transitional_bilingual: 14.83, number_transitional_bilingual: 37, percent_frl: 37.34, number_frl: 145, percent_foster_care: 2.04, number_foster_care: 31, student_enrollment_id: se.id)
+      ExceptionalStudentService.create(percent_special_education: 10, number_special_education: 181, percent_504: 7.56, number_504: 147, student_enrollment_id: se.id)
+      RaceEthnicity.create(percent_american_indian_or_alaskan_native: 2.03, number_american_indian_or_alaskan_native: 28, percent_asian: 14.30, number_asian: 94, percent_pacific_islander: 2.32, number_pacific_islander: 83, percent_asian_pacific_islander: 8.32, number_asian_pacific_islander: 23, percent_black: 42.12, number_black: 123, percent_hispanic: 13.42, number_hispanic: 983, percent_white: 12, number_white: 193, percent_two_or_more: 12.3, number_two_or_more: 412, student_enrollment_id: se.id)
+
+      get :show, slug: 'fake-school-district', year: "2015-16", format: :json
+
+      expected_response = {
+        message: "We do not have data for that combination of school district and school year. Please try another query.",
+        status: 400
+      }.to_json
+
+      expect(response.body).to eq(expected_response)
+    end
   end
 end
