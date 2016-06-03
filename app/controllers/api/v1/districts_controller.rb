e8a25492fb37c15801e1c7e549450ee13e04a487
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-03 14:53:13 -0600

    Add api key to district index api call

--- app/controllers/api/v1/districts_controller.rb
+++ app/controllers/api/v1/districts_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::DistrictsController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with District.all
+    key = params[:api_key]
+    if authenticated_api_key?(key)
+      respond_with District.all
+    else
+      respond_with unauthorized_response, status: 401, head: :unauthorized
+    end
   end
 end
--- spec/controller/districts_controller_spec.rb
+++ spec/controller/districts_controller_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe Api::V1::DistrictsController, type: :controller do
   describe "GET index" do
     it "returns all districts and their slugs" do
+      user = User.create(email: "example@example.com", password: "password", api_key: "abc123")
       county = County.create(name: "King", number: "17", slug: "king")
       educational_service_district = EducationalServiceDistrict.create(name: "Puget Sound Educational Service District 121", slug: "puget-sound-educational-service-district-121")
       district1 = District.create(name: "Auburn School District", number: "17408", educational_service_district_id: educational_service_district.id, county_id: county.id, slug: "auburn-school-district")
       district2 = District.create(name: "Issaquah School District", number: "17411", educational_service_district_id: educational_service_district.id, county_id: county.id, slug: "issaquah-school-district")
 
-      get :index, format: :json
+      get :index, api_key: user.api_key, format: :json
 
       districts = JSON.parse(response.body)
 
@@ -38,6 +39,20 @@ RSpec.describe Api::V1::DistrictsController, type: :controller do
                   }]
 
       expect(districts).to eq(result)
+    end
+
+    it "requires a valid API key" do
+      county = County.create(name: "King", number: "17", slug: "king")
+      educational_service_district = EducationalServiceDistrict.create(name: "Puget Sound Educational Service District 121", slug: "puget-sound-educational-service-district-121")
+      District.create(name: "Auburn School District", number: "17408", educational_service_district_id: educational_service_district.id, county_id: county.id, slug: "auburn-school-district")
+
+      get :index, format: :json
+
+      expected_response = { message: "Please include a valid API key with your request", status: 401 }.to_json
+
+      expect(response.code).to eq("401")
+      expect(expected_response).to eq(response.body)
+
     end
   end
 end
