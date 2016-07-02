83dcd58498197087e00ef757a2196859c0560641
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-07-02 16:37:17 -0700

    Spike total number of students by demographic in year

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,4 +5,12 @@ class State
                       .where(school_year_id: school_year.id)
                       .sum('student_enrollments.total')
   end
+
+  def self.number_and_percent_students_of_demographic_in_school_year(years, student_identifier_name)
+    school_year = SchoolYear.find_by(years: years)
+    PopulationDemographic.joins(:student_identifier, :district_school_year)
+                         .where(student_identifiers: { name: student_identifier_name }, district_school_years: {school_year_id: school_year.id})
+                         .sum(:number)
+
+  end
 end
