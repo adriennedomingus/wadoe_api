7baf1f8d5bca4a60a68f645f2c0ce5036250205c
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-08-06 09:44:05 -0700

    Write county highest graduation tests

--- spec/controller/graduation/counties_controller_spec.rb
+++ spec/controller/graduation/counties_controller_spec.rb
@@ -88,4 +88,73 @@ RSpec.describe Api::V1::Graduation::CountiesController, type: :controller do
       expect(expected_response).to eq(response.body)
     end
   end
+
+  describe "GET highest" do
+    it "responds with x districts with highest cohort graduation rates" do
+      school_year, county, county2, county3 = create_graduation_rates_for_multiple_counties
+      User.create(email: "example@example.com", password: "password", api_key: "abc123")
+
+      get :highest, identifier: 'female', year: school_year.years, top: 3, api_key: 'abc123', format: :json
+
+      top_graduation_rates = JSON.parse(response.body)
+
+      expect(top_graduation_rates.length).to eq(3)
+      expect(top_graduation_rates[0]["county"]["name"]).to eq(county.name)
+      expect(top_graduation_rates[1]["county"]["name"]).to eq(county2.name)
+      expect(top_graduation_rates[2]["county"]["name"]).to eq(county3.name)
+    end
+
+    it "requires a valid API key" do
+      school_year, _ = create_graduation_rates_for_multiple_counties
+
+      get :highest, identifier: 'female', year: school_year.years, top: 3, format: :json
+
+      expected_response = { message: "Please include a valid API key with your request", status: 401 }.to_json
+      expect(response.code).to eq("401")
+      expect(expected_response).to eq(response.body)
+    end
+
+    it "responds with overall data if identifier is not provided" do
+      school_year, county, county2, county3 = create_graduation_rates_for_multiple_counties
+      User.create(email: "example@example.com", password: "password", api_key: "abc123")
+
+      get :highest, year: school_year.years, top: 3, api_key: 'abc123', format: :json
+
+      top_graduation_rates = JSON.parse(response.body)
+
+      expect(top_graduation_rates.length).to eq(3)
+      expect(top_graduation_rates[0]["county"]["name"]).to eq(county.name)
+      expect(top_graduation_rates[0]["graduation"][0]["all"]).to be_truthy
+      expect(top_graduation_rates[1]["county"]["name"]).to eq(county2.name)
+      expect(top_graduation_rates[1]["graduation"][0]["all"]).to be_truthy
+      expect(top_graduation_rates[2]["county"]["name"]).to eq(county3.name)
+      expect(top_graduation_rates[2]["graduation"][0]["all"]).to be_truthy
+    end
+
+    it "respoonds with an error if the school year doesn't exist" do
+      User.create(email: "example@example.com", password: "password", api_key: "abc123")
+      create_graduation_rates_for_multiple_districts
+
+      get :highest, year: "2010-11", top: 3, api_key: 'abc123', format: :json
+
+      expected_response = {
+        message: "We do not have data for that school year. Please try another query.",
+        status: 404
+      }.to_json
+
+      expect(response.body).to eq(expected_response)
+      expect(response.code).to eq("404")
+    end
+
+    it "defaults to top ten if number is not provided" do
+      school_year, _ = create_graduation_rates_for_multiple_districts
+      User.create(email: "example@example.com", password: "password", api_key: "abc123")
+
+      get :highest, identifier: 'female', year: school_year.years, api_key: 'abc123', format: :json
+
+      top_graduation_rates = JSON.parse(response.body)
+      #test data currently only contains 5 districts. Returns 10 in dev
+      expect(top_graduation_rates.length).to eq(5)
+    end
+  end
 end
